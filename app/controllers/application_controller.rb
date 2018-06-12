d97078a2b639f496b69ea662c0c118801c170752
danieldevos <devos.daniel@telenet.be>
2018-06-12 16:19:27 +0200

    Modification des vues devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,6 @@ class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
         
-    end
-    
-
     protected
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
