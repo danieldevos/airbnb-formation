7a51b78241c1fba40308976fdca09b673405913e
danieldevos <devos.daniel@telenet.be>
2018-06-17 20:26:10 +0200

    Page modification de profil

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :path=>'',
-                     :path_names=>{:sign_in=>'Login', :sign_out=>'Logout', :edit=>'profile'} 
+                     :path_names=>{:sign_in=>'Login', :sign_out=>'Logout', :edit=>'profile'},
+                     :controllers=>{:registrations=>'registrations'} 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 # You can have the root of your site routed with "root"
